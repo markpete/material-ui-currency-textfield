24c8381dc0c688c255a788724fa88895bda415d8
Philipp Wirtenberger <Philipp.Wirtenberger@actus-tax.com>
2021-09-19 22:16:36 +0200

    upgrade to material-ui v5

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,9 @@
 import React from "react"
 import CurrencyTextField from "@unicef/material-ui-currency-textfield"
-import { Button } from "@material-ui/core"
+import { Button } from "@mui/material"
+import { createTheme, ThemeProvider } from "@mui/material/styles"
+
+const defaultTheme = createTheme();
 
 export default function App() {
   const [value, setValue] = React.useState(99)
@@ -11,20 +14,22 @@ export default function App() {
   }
 
   return (
-    <React.Fragment>
-      <CurrencyTextField
-        label="Amount"
-        value={value}
-        currencySymbol="$"
-        maximumValue={"100000000000000000"}
-        autoFocus
-        onChange={(e, value) => setValue(value)}
-        error={error}
-        helperText={"minimum number is 100"}
-        decimalCharacter="."
-        digitGroupSeparator=","
-      />
-      <Button onClick={resetValue}>Reset</Button>
-    </React.Fragment>
+    <ThemeProvider theme={defaultTheme}>
+      <React.Fragment>
+        <CurrencyTextField
+          label="Amount"
+          value={value}
+          currencySymbol="$"
+          maximumValue={"100000000000000000"}
+          autoFocus
+          onChange={(e, value) => setValue(value)}
+          error={error}
+          helperText={"minimum number is 100"}
+          decimalCharacter="."
+          digitGroupSeparator=","
+        />
+        <Button onClick={resetValue}>Reset</Button>
+      </React.Fragment>
+    </ThemeProvider>
   )
 }
